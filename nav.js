9b006c46d21d620ad365e2539907e91a79d39230
Wasik99 <229078@edu.p.lodz.pl>
2023-04-15 20:56:23 +0200

    mobile nav

---
+++ nav.js
@@ -0,0 +1,9 @@
+/* Set the width of the sidebar to 250px (show it) */
+const openNav = () => {
+    document.getElementById("sidepanel").style.width = "250px";
+    }
+
+    /* Set the width of the sidebar to 0 (hide it) */
+    const closeNav = () => {
+    document.getElementById("sidepanel").style.width = "0";
+    }
